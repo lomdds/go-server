a14b1d7b9fcc8e285c7dd4aded4221480514f49f
MrBergut <torgovickijdana@email.com>
2025-04-26 23:35:21 +0300

    Added authorization from another project and linked a database to it.

--- main.go
+++ main.go
@@ -1,40 +1,111 @@
 package main
 
 import (
-    "log"
-    "go-server/models"
-	"gorm.io/gorm"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/form3tech-oss/jwt-go"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"go-server/models"
 )
 
-func main() {
-    dsn := "host=localhost user=gorm password=gorm dbname=gorm port=5432 sslmode=disable"
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+var (
+	db           *gorm.DB
+	mySigningKey = []byte("secret")
+)
+
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API is up and running"))
+})
+
+var ArticlesHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    var articles []models.Article
+    if err := db.Find(&articles).Error; err != nil {
+        http.Error(w, "Failed to get articles", http.StatusInternalServerError)
+        return
+    }
+
+    payload, err := json.Marshal(articles)
     if err != nil {
-        log.Fatal("Не удалось подключиться к БД:", err)
+        http.Error(w, "Failed to marshal articles", http.StatusInternalServerError)
+        return
     }
+    
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(payload)
+})
 
-    // Пример создания пользователя
-    user := models.User{
-        Username: "John Doe",
-        Email:    "ololo@gmail.com",
-        Password: "ololo123",
-    }
-    if err := db.Create(&user).Error; err != nil {
-        log.Fatal("Ошибка создания пользователя:", err)
-    }
+var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return mySigningKey, nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
 
-    // Обновление пользователя
-    if err := db.Model(&user).Updates(models.User{
-        Username: "Mussolini",
-        Email:    "Italy@gmail.com",
-        Password: "123",
-    }).Error; err != nil {
-        log.Fatal("Ошибка обновления:", err)
-    }
+func GetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		http.Error(w, "Username is required", http.StatusBadRequest)
+		return
+	}
 
-    // Удаление пользователя
-    if err := db.Delete(&user).Error; err != nil {
-        log.Fatal("Ошибка удаления:", err)
-    }
+	var user models.User
+	result := db.Where("username = ?", username).First(&user)
+	if result.Error != nil {
+		http.Error(w, "User not found", http.StatusUnauthorized)
+		return
+	}
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = user.ID
+	claims["username"] = user.Username
+	claims["email"] = user.Email
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": tokenString,
+	})
+}
+
+func main() {
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Не удалось подключиться к БД:", err)
+	}
+
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatal("Ошибка миграции:", err)
+	}
+
+	r := mux.NewRouter()
+
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	r.Handle("/status", StatusHandler).Methods("GET")
+	r.Handle("/get-token", http.HandlerFunc(GetTokenHandler)).Methods("GET")
+	r.Handle("/articles", jwtMiddleware.Handler(ArticlesHandler)).Methods("GET")
+
+	log.Println("Server starting on :3000")
+	http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
 }
